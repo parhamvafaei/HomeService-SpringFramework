c090268261af15b9c5228b4bb15824bb7f157442
parham <p.vafaeei@gmail.com>
2022-12-22 22:54:19 +0330

    Create Offer class

---
+++ src/main/java/com/maktab/entity/Offer.java
@@ -0,0 +1,31 @@
+package com.maktab.entity;
+
+import com.maktab.base.entity.BaseEntity;
+import com.maktab.entity.person.Expert;
+import lombok.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import java.time.Duration;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Offer extends BaseEntity {
+
+    private Double price;
+    private Duration durationTime;
+
+    @ManyToOne
+    @NotNull
+    private Expert expert;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Order order;
+
+}
