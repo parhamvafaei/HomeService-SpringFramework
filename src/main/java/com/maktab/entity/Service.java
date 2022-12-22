f1e92e61bf6db69375fdf5b76e4e134ab67ac453
parham <p.vafaeei@gmail.com>
2022-12-22 22:55:43 +0330

    Create Servisw class

---
+++ src/main/java/com/maktab/entity/Service.java
@@ -0,0 +1,24 @@
+package com.maktab.entity;
+
+import com.maktab.base.entity.BaseEntity;
+import lombok.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Service extends BaseEntity {
+    private String name;
+
+    @OneToMany(mappedBy = "service", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<SubService> subServices;
+
+}
