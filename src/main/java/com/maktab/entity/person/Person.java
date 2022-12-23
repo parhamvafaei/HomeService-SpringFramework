e1e5588569fb02ba83e054bbf5cd80dfccd24cfb
parham <p.vafaeei@gmail.com>
2022-12-23 11:57:13 +0330

    create Person class

---
+++ src/main/java/com/maktab/entity/person/Person.java
@@ -0,0 +1,34 @@
+package com.maktab.entity.person;
+
+
+import com.maktab.base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public abstract class Person extends BaseEntity {
+    private String firstName;
+    private String firstLast;
+
+    @Email  @Column(unique = true)
+    private String Email;
+
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[A-Z])(?=\\S+$).{8}")
+    private String password;
+
+}
