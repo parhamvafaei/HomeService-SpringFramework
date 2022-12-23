ddb2b17fa244f431be9914743e0e5b19a92b78fc
parham <p.vafaeei@gmail.com>
2022-12-23 12:00:43 +0330

    create AdminRepository class

---
+++ src/main/java/com/maktab/repository/AdminRepository.java
@@ -0,0 +1,15 @@
+package com.maktab.repository;
+
+
+import com.maktab.entity.person.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+
+
+
+
+}
