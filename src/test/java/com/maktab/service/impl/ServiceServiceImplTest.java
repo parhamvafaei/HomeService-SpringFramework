497eaf714d105ad50b443f6a79b7f0f2bb312de2
parham <p.vafaeei@gmail.com>
2022-12-23 12:08:24 +0330

    create ServiceServiceImplTest class

---
+++ src/test/java/com/maktab/service/impl/ServiceServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.maktab.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ServiceServiceImplTest {
+
+    @Test
+    void loadServices() {
+    }
+}
