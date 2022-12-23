f562a20e073bddda66b3df613f65b6e1580c6f5a
parham <p.vafaeei@gmail.com>
2022-12-23 12:02:02 +0330

    create ServiceRepository class

---
+++ src/main/java/com/maktab/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.maktab.repository;
+
+
+import com.maktab.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service,Long> {
+
+}
