542a40fb3af08ee3ccb56d4e1f4c4f4c45a6db63
parham <p.vafaeei@gmail.com>
2022-12-23 12:08:02 +0330

    create SubServiceServiceImplTest class

---
+++ src/test/java/com/maktab/service/impl/AdminServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.maktab.service.impl;
+
+import com.maktab.entity.person.Admin;
+import com.maktab.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AdminServiceImplTest {
+
+    @Autowired
+    private AdminService adminService;
+
+
+    @Test
+    void changePassword() {
+        Admin admin = new Admin();
+        admin.setPassword("Pksdjfj2");
+        adminService.saveOrUpdate(admin);
+        adminService.changePassword(admin.getId(), "FGsrofm3");
+
+
+        assertEquals("FGsrofm3", adminService.findById(admin.getId()).get().getPassword());
+    }
+
+}
---
+++ src/test/java/com/maktab/service/impl/SubServiceServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.maktab.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SubServiceServiceImplTest {
+
+    @Test
+    void checkSubServiceInName() {
+    }
+
+    @Test
+    void addSubService() {
+    }
+
+    @Test
+    void loadSubServices() {
+    }
+
+    @Test
+    void editSubService() {
+    }
+
+    @Test
+    void addExpertToSubService() {
+    }
+
+    @Test
+    void deleteExpertOfSubService() {
+    }
+
+}
