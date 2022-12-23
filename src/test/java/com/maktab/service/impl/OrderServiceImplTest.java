b2e6d55706b5c87eda642e7e30f72c9eb4f44519
parham <p.vafaeei@gmail.com>
2022-12-23 12:09:14 +0330

    create OrderServiceImplTest class

---
+++ src/test/java/com/maktab/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.maktab.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class OrderServiceImplTest {
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void chooseExpert() {
+    }
+
+    @Test
+    void changeOrderStatusToStarted() {
+    }
+
+    @Test
+    void changeOrderStatusToDone() {
+    }
+}
