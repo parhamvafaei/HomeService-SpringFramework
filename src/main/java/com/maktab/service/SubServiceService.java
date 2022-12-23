394f7490ab51bb926b460bd463ec8dba6c4bd88f
parham <p.vafaeei@gmail.com>
2022-12-23 12:07:27 +0330

    create SubServiceService class

---
+++ src/main/java/com/maktab/service/SubServiceService.java
@@ -0,0 +1,25 @@
+package com.maktab.service;
+
+
+import com.maktab.base.service.impl.BaseService;
+import com.maktab.entity.Service;
+import com.maktab.entity.SubService;
+import com.maktab.entity.person.Expert;
+
+import java.util.List;
+
+// how to write query when use single table in inheritance
+public interface SubServiceService extends BaseService<SubService> {
+    Boolean checkSubServiceInName(String name);
+
+    Long addSubService(SubService subService, Service service);
+
+    List<SubService> LoadSubServices();
+
+    void editSubService(Long id, Double price, String description);
+
+    void addExpertToSubService(Expert expert, SubService subService);
+
+    void deleteExpertOfSubService(Expert expert, SubService subService);
+
+}
