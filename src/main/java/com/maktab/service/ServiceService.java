d33a0937a0aad2c1285e525657bcfebfe9961a06
parham <p.vafaeei@gmail.com>
2022-12-23 12:06:56 +0330

    create ServiceService class

---
+++ src/main/java/com/maktab/service/ServiceService.java
@@ -0,0 +1,13 @@
+package com.maktab.service;
+
+
+import com.maktab.base.service.impl.BaseService;
+import com.maktab.entity.Service;
+
+import java.util.List;
+
+public interface ServiceService extends BaseService<Service> {
+
+
+    List<Service> loadServices();
+}
