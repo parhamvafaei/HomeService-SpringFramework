e8f27cd978c9813b629d2db154f0a03ec71494e3
parham <p.vafaeei@gmail.com>
2022-12-23 12:03:30 +0330

    create BaseServiceImpl class

---
+++ src/main/java/com/maktab/base/service/impl/BaseServiceImpl.java
@@ -0,0 +1,47 @@
+package com.maktab.base.service.impl;
+
+
+import com.maktab.base.entity.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BaseServiceImpl<E extends BaseEntity, R extends JpaRepository<E, Long>> implements BaseService<E> {
+
+    protected final R repository;
+
+    public BaseServiceImpl(R repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @Transactional
+    public Long saveOrUpdate(E e) {
+        repository.save(e);
+        return e.getId();
+    }
+
+
+    @Override
+    public void delete(E e) {
+        repository.delete(e);
+    }
+
+    @Override
+    public Optional<E> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<E> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public boolean isExistsById(Long id) {
+        return repository.existsById(id);
+
+    }
+}
