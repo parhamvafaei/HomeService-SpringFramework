3185219494fc0ac0a3fd8a214464db1bb217fcdd
parham <p.vafaeei@gmail.com>
2022-12-29 23:38:12 +0330

    create ClientDTO to use in Controller

---
+++ src/main/java/com/maktab/entity/dto/ClientDTO.java
@@ -0,0 +1,26 @@
+package com.maktab.entity.dto;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ClientDTO {
+
+    private String firstName;
+    private String lastName;
+    @Email
+    @Column(unique = true)
+    private String email;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[A-Z])(?=\\S+$).{8}")
+    @NotNull
+    private String password;
+}
