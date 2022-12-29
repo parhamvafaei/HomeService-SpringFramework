a51664bb495aac694dbdb7fee2dfbcc763f60f91
parham <p.vafaeei@gmail.com>
2022-12-29 23:49:26 +0330

    add showRelatedOrdersBySubService which combined with two methods

--- src/main/java/com/maktab/service/OrderService.java
+++ src/main/java/com/maktab/service/OrderService.java
@@ -5,6 +5,7 @@ import com.maktab.base.service.BaseService;
 import com.maktab.entity.Address;
 import com.maktab.entity.Order;
 import com.maktab.entity.SubService;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.List;
 public interface OrderService extends BaseService<Order> {
     Long addOrder(Double price, String description, LocalDateTime time, Address address, SubService subService);
 
-    void chooseExpert(Long id);
+
+
+    void selectExpertToOrder(Long offerId, Long orderId);
 
     void changeOrderStatusToStarted(Long id);
 
@@ -20,5 +23,4 @@ public interface OrderService extends BaseService<Order> {
 
     List<Order> showRelatedOrdersBySubService(Long expertId);
 
-    List<Order> findOrdersToOffer();
 }
--- src/main/java/com/maktab/service/impl/OrderServiceImpl.java
+++ src/main/java/com/maktab/service/impl/OrderServiceImpl.java
@@ -4,25 +4,34 @@ package com.maktab.service.impl;
 import com.maktab.base.service.impl.BaseServiceImpl;
 import com.maktab.entity.*;
 import com.maktab.entity.person.Expert;
+import com.maktab.exception.ExpertConditionException;
 import com.maktab.exception.NotFoundPersonException;
 import com.maktab.exception.OrderStatusConditionException;
+import com.maktab.exception.SelectOrderException;
 import com.maktab.repository.OrderRepository;
 import com.maktab.service.ExpertService;
+import com.maktab.service.OfferService;
 import com.maktab.service.OrderService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class OrderServiceImpl extends BaseServiceImpl<Order, OrderRepository> implements OrderService {
 
     private final ExpertService expertService;
-    public OrderServiceImpl(OrderRepository repository, ExpertService expertService) {
+    private final OfferService offerService;
+
+    public OrderServiceImpl(OrderRepository repository, @Lazy ExpertService expertService, @Lazy OfferService offerService) {
         super(repository);
         this.expertService = expertService;
+        this.offerService = offerService;
     }
 
     @Transactional
@@ -33,20 +42,25 @@ public class OrderServiceImpl extends BaseServiceImpl<Order, OrderRepository> im
         order.setSubService(subService);
 
         saveOrUpdate(order);
-return order.getId();
+        return order.getId();
     }
 
-    //    save Expert to order
     @Transactional
     @Override
-    public void chooseExpert(Long id) {
-        Order order = findById(id).orElseThrow(NullPointerException::new);
-        if (order.getOrderStatus().equals(OrderStatus.SELECTING_EXPERT)) {
-            order.setOrderStatus(OrderStatus.WAITING_EXPERT_COME);
-            saveOrUpdate(order);
-        } else
-            throw new OrderStatusConditionException();
+    public void selectExpertToOrder(Long offerId, Long orderId) {
+
+        Order order = findById(orderId).orElseThrow(NullPointerException::new);
+        Offer offer = offerService.findById(offerId).orElseThrow(NullPointerException::new);
+        if (!(offer.getOrder().getId().equals(order.getId())))
+            throw new SelectOrderException();
+        if (!(order.getOrderStatus().equals(OrderStatus.SELECTING_EXPERT)))
+            throw new ExpertConditionException();
+        order.setTime(LocalDateTime.now());
+        order.setOrderStatus(OrderStatus.WAITING_FOR_EXPERT);
+        offerService.saveOrUpdate(offer);
+        saveOrUpdate(order);
     }
+
 
     @Transactional
     @Override
@@ -74,21 +88,19 @@ return order.getId();
     @Override
     public List<Order> showRelatedOrdersBySubService(Long expertId) {
         Expert expert = expertService.findById(expertId).orElseThrow(NotFoundPersonException::new);
-        List<Order> result =new ArrayList<>();
+
+        List<Order> relatedList = new ArrayList<>();
         List<SubService> subServices = expert.getSubServices();
-        subServices.forEach(subService -> result.addAll(subService.getOrders()));
-        return result;
+        subServices.forEach(subService -> relatedList.addAll(subService.getOrders()));
 
-    }
 
-@Override
-    public  List<Order> findOrdersToOffer() {
-        return findAll().stream()
+        return relatedList.stream()
                 .filter(order -> order
                         .getOrderStatus()
                         .equals(OrderStatus.WAITING_FOR_EXPERT))
                 .toList();
 
     }
+
 
 }
