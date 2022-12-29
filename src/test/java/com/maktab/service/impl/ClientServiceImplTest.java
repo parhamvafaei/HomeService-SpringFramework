0a5e615f2e14b1ae8c4899a4adc94f8826a94fe0
parham <p.vafaeei@gmail.com>
2022-12-29 23:44:25 +0330

    clean coding ClientServiceImplTest class

--- src/test/java/com/maktab/service/impl/ClientServiceImplTest.java
+++ src/test/java/com/maktab/service/impl/ClientServiceImplTest.java
@@ -3,26 +3,36 @@ package com.maktab.service.impl;
 import com.maktab.entity.person.Admin;
 import com.maktab.entity.person.Client;
 import com.maktab.service.ClientService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ClientServiceImplTest {
 
     @Autowired
     private ClientService service;
 
     @Test
+    @Order(1)
+    void signIn() {
+        Long id = service.signIn("parham", "vafaei", "parhm@gmail.com", "Parham12");
+        assertEquals(1L, id);
+
+    }
+
+    @Test
+    @Order(2)
     void changePassword() {
 
-        Client client=new Client();
-        client.setPassword("Pksdjfj2");
-        service.saveOrUpdate(client);
-        service.changePassword(client.getId(), "FGsrofm3");
+        service.changePassword(1L, "Ppppppp2");
 
-        assertEquals("FGsrofm3", service.findById(client.getId()).get().getPassword());
+        assertEquals("Ppppppp2", service.findById(1L).get().getPassword());
     }
-    }
+}
