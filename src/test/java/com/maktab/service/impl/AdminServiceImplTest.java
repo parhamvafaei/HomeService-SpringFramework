ecfadec3ea060669236f329ca11adb063b444bc7
parham <p.vafaeei@gmail.com>
2022-12-29 23:43:54 +0330

    clean coding ExpertServiceImplTest class

--- src/test/java/com/maktab/service/impl/AdminServiceImplTest.java
+++ src/test/java/com/maktab/service/impl/AdminServiceImplTest.java
@@ -3,34 +3,38 @@ package com.maktab.service.impl;
 import com.maktab.entity.person.Admin;
 import com.maktab.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AdminServiceImplTest {
 
     @Autowired
     private AdminService adminService;
 
-
     @Test
-    void changePassword() {
-        Admin admin = new Admin();
-        admin.setPassword("Pksdjfj2");
-        adminService.saveOrUpdate(admin);
-        adminService.changePassword(admin.getId(), "FGsrofm3");
+    @Order(1)
+    void createAdmin() {
 
+        Long id = adminService.createAdmin("parham", "vafaei", "parhm@gmail.com", "Parham12");
 
-        assertEquals("FGsrofm3", adminService.findById(admin.getId()).get().getPassword());
+        assertEquals(1L, id);
     }
 
+
     @Test
-    void createAdmin() {
-        adminService.createAdmin("padfef","aeff","Parham_fqe2@gamil.com","Prsgmdf3");
+    @Order(2)
+    void changePassword() {
+        adminService.changePassword(1L, "Parham12");
 
 
+        assertEquals("Parham12", adminService.findById(1L).get().getPassword());
     }
 }
