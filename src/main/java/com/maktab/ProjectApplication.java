0630fc9a13f93f0189e1736bc9bfa49198c1f1ce
parham <p.vafaeei@gmail.com>
2022-12-31 15:13:48 +0330

    update

--- src/main/java/com/maktab/ProjectApplication.java
+++ src/main/java/com/maktab/ProjectApplication.java
@@ -1,22 +1,10 @@
 package com.maktab;
 
 
-import com.maktab.entity.Service;
-import com.maktab.entity.SubService;
-import com.maktab.entity.person.Admin;
-import com.maktab.entity.person.Client;
-import com.maktab.entity.person.Expert;
-import com.maktab.entity.person.ExpertStatus;
-import com.maktab.service.*;
-import org.apache.catalina.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-
-
-import java.util.List;
-
 
 @SpringBootApplication
 public class ProjectApplication {
