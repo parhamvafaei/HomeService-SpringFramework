6efce35e6dc03435ba07f36fb3c03f0c5a1d64b9
parham <p.vafaeei@gmail.com>
2023-01-01 14:46:15 +0330

    change address field to AddressDTO

--- src/main/java/com/maktab/entity/dto/OrderDTO.java
+++ src/main/java/com/maktab/entity/dto/OrderDTO.java
@@ -1,5 +1,6 @@
 package com.maktab.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.maktab.entity.Address;
 import lombok.*;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 public class OrderDTO {
     private Double price;
     private String description;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time;
-    private Address address;
+    private AddressDTO address;
 }
