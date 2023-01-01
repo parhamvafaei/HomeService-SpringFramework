d76a4477863a0702119e7177b75b6e26e2ab2cf2
parham <p.vafaeei@gmail.com>
2023-01-01 14:44:21 +0330

    change OrderAddress relation to bidirectional

--- src/main/java/com/maktab/entity/Address.java
+++ src/main/java/com/maktab/entity/Address.java
@@ -5,6 +5,8 @@ import com.maktab.base.entity.BaseEntity;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,5 +19,7 @@ public class Address extends BaseEntity {
     private String address;
     private Long phoneNumber;
 
+    @OneToMany(mappedBy = "address")
+    private List<Order> orders;
 
 }
