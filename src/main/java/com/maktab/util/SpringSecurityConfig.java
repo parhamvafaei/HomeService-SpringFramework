e04a8e3812807c1f7e23cac6b15e34a03a6119d6
parham <p.vafaeei@gmail.com>
2023-01-10 17:03:19 +0330

    update

--- src/main/java/com/maktab/util/SpringSecurityConfig.java
+++ src/main/java/com/maktab/util/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.maktab.util;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,13 @@ public class SpringSecurityConfig {
 //        return null;
 //    }
 
+
+    @Bean
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("user")
+                .password(passwordEncoder().encode("password")).roles("USER");
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
