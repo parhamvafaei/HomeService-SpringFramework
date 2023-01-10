bff394666a932417c9d9629ecf7ea7f63e57f68e
parham <p.vafaeei@gmail.com>
2023-01-10 15:06:00 +0330

    update and implement from UserDetail interface

--- src/main/java/com/maktab/entity/person/Client.java
+++ src/main/java/com/maktab/entity/person/Client.java
@@ -4,6 +4,8 @@ package com.maktab.entity.person;
 import com.maktab.entity.Credit;
 import com.maktab.entity.Order;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -13,6 +15,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -21,7 +24,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Client extends Person {
+public class Client extends Person implements UserDetails {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Credit credit;
@@ -35,5 +38,30 @@ public class Client extends Person {
     public Client(String firstName, String lastName, String username, @Email String email, @Pattern(regexp = "(?=.*[0-9])(?=.*[A-Z])(?=\\S+$).{8}") @NotNull String password, Credit credit) {
         super(firstName, lastName, username, email, password);
         this.credit = credit;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
