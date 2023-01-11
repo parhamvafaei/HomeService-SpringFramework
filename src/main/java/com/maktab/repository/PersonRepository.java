c6a4978beced1f552ff49a4d586eb1b223407337
parham <p.vafaeei@gmail.com>
2023-01-11 19:35:12 +0330

    create person repository

---
+++ src/main/java/com/maktab/repository/PersonRepository.java
@@ -0,0 +1,25 @@
+package com.maktab.repository;
+
+
+import com.maktab.entity.person.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
+public interface PersonRepository<E extends Person> extends JpaRepository<E,Long> {
+
+
+    @Query("from Person p where p.email =:email")
+    Optional<E> findByEmail(String email);
+
+
+    @Transactional
+    @Modifying
+    @Query("update Person p set p.enabled=true where p.email=:email")
+    int enableAppUser(String email);
+}
