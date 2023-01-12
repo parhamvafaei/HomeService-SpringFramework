0667956c1beb3cebcdbb41049c206cb60be34ebb
parham <p.vafaeei@gmail.com>
2023-01-12 20:03:47 +0330

    create OrderIdDTO class

---
+++ src/main/java/com/maktab/entity/dto/OrderDTOPay.java
@@ -0,0 +1,14 @@
+package com.maktab.entity.dto;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class OrderDTOPay {
+
+    private Long order_id ;
+}
